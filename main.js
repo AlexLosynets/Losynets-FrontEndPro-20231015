818df9b44a650c9aeb3100fca0735a18d0753e66
AlexLosynets <42945876+AlexLosynets@users.noreply.github.com>
2023-10-15 23:58:32 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-const YourName = window.prompt("Enter your name", "John");
+const YourName = window.prompt("Вкажіть своє імʼя", "Іванка");
+const YourMidname = window.prompt("Вкажіть своє по-батькові", "Іванівна");
+const YourLastname = window.prompt("Вкажіть своє прізвище", "Іванченко")
 
-alert ('Hello, ' + YourName + "! How are you?")
+alert ('Вітаю тебе, ' + YourLastname + " " + YourName + " " + YourMidname + "!")
+
+let YourNumber = window.prompt("Вкажи пʼятизначне число", "12345");
+StringYourNumber = String(YourNumber);
+
+alert(StringYourNumber.split("")[0] + " " + StringYourNumber.split("")[1]  + " "  + StringYourNumber.split("")[2] + " "  + StringYourNumber.split("")[3] + " " + StringYourNumber.split("")[4])
