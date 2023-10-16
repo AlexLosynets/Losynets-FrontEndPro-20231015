6ee3b91caca94722ce44a34e553952d52596c469
AlexLosynets <42945876+AlexLosynets@users.noreply.github.com>
2023-10-16 18:53:02 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-let NumberA = window.prompt("Введіть перше число", "2");
-let NumberB = window.prompt("Введіть друге число", "2");
-const a = Number(NumberA);
-const b = Number(NumberB);
-const add = a+b;
-const sub = a-b;
-const multipl = a*b;
-const divi = a/b;
-
-alert ('Користувач ввів ' + a + " і " + b + ": " 
-+ a + "+" + b + "=" + add + "; "
-+ a + "-" + b + "=" + sub + "; "
-+ a + "*" + b + "=" + multipl + "; "
-+ a + "/" + b + "=" + divi + "."
-)
+let Hours = window.prompt("Введіть кількість годин", "2");
+const a = Number(Hours);
+const b = a*3600;
+alert ('Це ' + b + " секунд")
